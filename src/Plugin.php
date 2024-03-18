38f0d2472ac8eaf9b4ffc0c008d6951451b6c094
D. Nagy Gergő <hello@iamgergo.com>
2024-03-18 09:25:09 +0100

    add slug to constant

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,12 +12,19 @@ abstract class Plugin
     public const VERSION = '1.0.0';
 
     /**
+     * The plugin version.
+     *
+     * @var string
+     */
+    public const SLUG = 'plugin-slug';
+
+    /**
      * Boot the plugin.
      */
     public static function boot(): void
     {
         add_action('init', static function (): void {
-            load_plugin_textdomain('plugin-slug', false, basename(dirname(__DIR__)).'/languages');
+            load_plugin_textdomain(static::SLUG, false, basename(dirname(__DIR__)).'/languages');
         });
     }
 
